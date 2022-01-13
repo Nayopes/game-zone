ced2da986c50e132e6e28b13069976ccc9f9c3bd
Nayopes <mariam.sepoyan13@gmail.com>
2022-01-13 11:49:31 +0400

    work

--- assets/dom.js
+++ assets/dom.js
@@ -1,73 +1,32 @@
-particlesJS("particles-js", {
-    particles: {
-      number: { value: 160, density: { enable: true, value_area: 800 } },
-      color: { value: "#ffffff" },
-      shape: {
-        type: "circle",
-        stroke: { width: 0, color: "#000000" },
-        polygon: { nb_sides: 5 },
-        image: { src: "img/github.svg", width: 100, height: 100 }
-      },
-      opacity: {
-        value: 1,
-        random: true,
-        anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: { enable: false, speed: 4, size_min: 0.3, sync: false }
-      },
-      line_linked: {
-        enable: false,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 1,
-        direction: "none",
-        random: true,
-        straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: { enable: false, rotateX: 600, rotateY: 600 }
-      }
+let $header_top = $('.header-top');
+let $nav = $('nav');
+
+$header_top.find('a').on('click', function() {
+    $(this).parent().toggleClass('open-menu');
+});
+
+$('#fullpage').fullpage({
+    sectionsColor: ['#1aaa82', '#1f7889', '#03262f', '#081634', '#ffd03c'],
+    sectionSelector: '.vertical-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    anchors: ['home', 'games', 'gallery', 'contacts'],
+    menu: '#menu',
+
+    afterLoad: function(anchorLink, index) {
+        $header_top.css('background', 'rgba(0, 47, 77, .3)');
+        $nav.css('background', 'rgba(0, 47, 77, .25)');
+        if (index == 5) {
+            $('#fp-nav').hide();
+        }
     },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: { enable: true, mode: "bubble" },
-        onclick: { enable: true, mode: "repulse" },
-        resize: true
-      },
-      modes: {
-        grab: { distance: 400, line_linked: { opacity: 1 } },
-        bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
-        repulse: { distance: 400, duration: 0.4 },
-        push: { particles_nb: 4 },
-        remove: { particles_nb: 2 }
-      }
+
+    onLeave: function(index, nextIndex, direction) {
+        if(index == 4) {
+            $('#fp-nav').show();
+        }
     },
-    retina_detect: true
-  });
-  var count_particles, stats, update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector(".js-count-particles");
-  update = function () {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
-  
+
+
+});
