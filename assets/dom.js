f30613a62fff2d0454febb523630587df1ebbaec
Mariam Sepoyan <68228432+Nayopes@users.noreply.github.com>
2022-02-02 18:10:13 +0400

    Update dom.js

--- assets/dom.js
+++ assets/dom.js
@@ -1,1 +1,16 @@
 
+
+const burgerMenu = document.getElementById('burgerMenu');
+const sideMenuMain = document.getElementById('sideMenuMain');
+let isOpen = false
+burgerMenu.onclick=()=>{
+    if(!isOpen){
+        isOpen = true;
+        sideMenuMain.style.maxHeight = 100+'vh';
+        sideMenuMain.style.fontSize = 14+'px';
+    }else{
+        sideMenuMain.style.maxHeight = 0;
+        sideMenuMain.style.fontSize =0;
+        isOpen = false;
+    }
+};
