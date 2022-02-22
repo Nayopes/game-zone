50c39c189768f0c6b62255048bb19a5b9fcca479
Nayopes <mariam.sepoyan13@gmail.com>
2022-02-22 12:56:01 +0400

    white back

---
+++ assets/dom.js
@@ -0,0 +1,15 @@
+
+const burgerMenu = document.getElementById('burgerMenu');
+const sideMenuMain = document.getElementById('sideMenuMain');
+let isOpen = false
+burgerMenu.onclick=()=>{
+    if(!isOpen){
+        isOpen = true;
+        sideMenuMain.style.maxHeight = 100+'vh';
+        sideMenuMain.style.fontSize = 14+'px';
+    }else{
+        sideMenuMain.style.maxHeight = 0;
+        sideMenuMain.style.fontSize =0;
+        isOpen = false;
+    }
+};
